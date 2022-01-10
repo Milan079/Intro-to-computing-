81546e3d44c5a33d52ecc3b63c23c1d7c81f23f4
Milan Agarwal <96536950+Milan079@users.noreply.github.com>
2022-01-10 23:16:12 +0530

    Add files via upload

---
+++ Assign_1_output.py
@@ -0,0 +1,33 @@
+========= RESTART: /Users/milanagarwal/Desktop/Computing/Assgn 1/Q1.py =========
+Enter the no 1: 45
+Enter the no 2: 56
+Enter the no 3: 78
+The average of numbers are:  59.666666666666664
+
+========= RESTART: /Users/milanagarwal/Desktop/Computing/Assgn 1/Q2.py =========
+Enter your income : 100000
+Enter the no of dependents : 6
+14400.0
+
+========= RESTART: /Users/milanagarwal/Desktop/Computing/Assgn 1/Q3.py =========
+Enter your  name: Milan Agarwal
+Enter your student id: 21103077
+Enter your gender: M
+Enter your course name: Intro to computing
+Enter your CGPA: 9.75
+['21103077', 'Milan Agarwal', 'M', 'Intro to computing', 9.75]
+
+========= RESTART: /Users/milanagarwal/Desktop/Computing/Assgn 1/Q4.py =========
+Enter student 1 marks: 56
+Enter student 2 marks: 78
+Enter student 3 marks: 68
+Enter student 4 marks: 89
+Enter student 5 marks: 20
+[56, 78, 68, 89, 20]
+sorted list of students marks :  [20, 56, 68, 78, 89]
+
+======== RESTART: /Users/milanagarwal/Desktop/Computing/Assgn 1/Q5 a.py ========
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+======== RESTART: /Users/milanagarwal/Desktop/Computing/Assgn 1/Q5 b.py ========
+['Red', 'Green', 'White', 'Purple', 'Yellow']
