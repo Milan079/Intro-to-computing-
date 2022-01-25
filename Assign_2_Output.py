6a777a590e9aeb80f04acaf4f6366d3b416139c9
Milan Agarwal <96536950+Milan079@users.noreply.github.com>
2022-01-25 23:00:38 +0530

    Add files via upload

---
+++ Assign_2_Output.py
@@ -0,0 +1,78 @@
+
+
+-----------------Q1----------------------------- 
+Part a: Length of the string =  35
+
+Part b: Here's the reverse order =
+ egaugnal evitisnes esac a si nohtyP
+
+Part c:Here's the new string =  a case sensitive
+
+
+Part d: Here's the replacement =  Python is  object oriented language
+
+
+Part e: Here's the index of 'a' = 10
+
+
+Part f: Here's the string without white spaces: Pythonisacasesensitivelanguage
+
+
+---------------Q2---------------------------
+Enter your name : ABC
+Enter your SID : 2110XXXX
+Enter your dept. : XYZ
+Enter your CGPA : 9.9
+Hey, ABC Here! 
+My SID is 2110XXXX 
+I am from XYZ department and my CGPA is 9.9
+
+
+
+-------------Q3-----------------------------
+
+Part a: a&b =  8
+
+
+Part b: a|b =  58
+
+Part c: a^b =  50
+
+Part d: Left shift a =  224
+Part d: Left shift b =  40
+
+
+Part e: Right shift a =  14
+Part e: Right shift b =  0
+
+
+------------------Q4------------------
+
+enter the no. 1:  12
+enter the no. 2 :  23
+enter the no. 3:  45
+Largest no. is :  45
+
+
+
+-----------------------Q5----------------------------
+
+
+Please type a statement:
+Hello my name is Milan
+Yes
+
+
+-------------------------------Q6---------------------
+
+Enter first side:
+12
+Enter second side:
+5
+Enter third side:
+13
+Yes
+
+
+
+------------------END-----------------------------
